f13fd10853fab46b93303c9e814063fb281f4a73
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-14 13:44:41 +0700

    get user claim in Profile Service

--- Identity.API/Service/CustomProfileService.cs
+++ Identity.API/Service/CustomProfileService.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Security.Claims;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
 using Identity.API.Entity;
+using IdentityModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,13 +20,28 @@ namespace Identity.API.Service
         }
         public Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            // get current user
-            var user = _userManager.GetUserAsync(context.Subject).Result?? throw new ArgumentException("Invalid subject identifier");
-            // get all user claims 
-            var claims = _userManager.GetClaimsAsync(user).Result;
+            var user = _userManager.GetUserAsync(context.Subject).Result ?? throw new ArgumentException("Invalid subject identifier");
+            var claims = GetClaims(user).Result;
             // add user claims to context
             context.IssuedClaims.AddRange(claims);
             return Task.CompletedTask;
+        }
+
+        public async Task<List<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.Subject, user.Id),
+                new Claim(JwtClaimTypes.Name, user.UserName??""),
+                new Claim(JwtClaimTypes.Email, user.Email??""),
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
         }
 
         public Task IsActiveAsync(IsActiveContext context)
